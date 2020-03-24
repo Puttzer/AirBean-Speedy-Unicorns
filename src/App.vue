0f3847aa65c05716b41a33cd8df8771a7e4482c6
Jenseneducation-Patrick-Carragher <53831562+Puttzer@users.noreply.github.com>
2020-03-24 15:51:50 +0100

    Revert "meny page"

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,32 @@
 <template>
   <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
     <router-view/>
   </div>
 </template>
 
-<script>
-export default {
-  created() {
-    this.$store.dispatch("getMenuList");
-  }
-}
-</script>
-
-
 <style lang="scss">
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-size: 10px;
-}
-
-html {
-  background: white;
-  max-width: 100vw;
-  height: auto;
-  padding: 2rem 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
 #app {
-  width: 37.5rem;
-  min-height: 66.7rem;
-  background:white;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
